1e9e1bfee7bb3f0529de51e61257936d89bdb5aa
roma_marshall <roma_marshall@outlook.com>
2022-07-06 17:39:19 +0200

    anonymous function inited

---
+++ script.js
@@ -0,0 +1,19 @@
+let texttop, textbottom, img, generate, meme;
+
+function createMemes () {
+
+}
+
+(() => {
+    texttop = document.querySelector('#text-top');
+    textbottom = document.querySelector('#text-bottom');
+    img = document.querySelector('#img');
+    create = document.querySelector('#generate');
+    meme = document.querySelector('#meme');
+
+    console.log(texttop)
+    console.log(textbottom)
+    console.log(img)
+    console.log(create)
+    console.log(meme)
+})()
