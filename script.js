7a0504aeb31388e47eaf990b06263277ae1018f1
roma_marshall <roma_marshall@outlook.com>
2022-07-06 17:42:03 +0200

    eventListener added

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let texttop, textbottom, img, generate, meme;
 
-function createMemes () {
-
-}
+function createMemes() {}
 
 (() => {
-    texttop = document.querySelector('#text-top');
-    textbottom = document.querySelector('#text-bottom');
-    img = document.querySelector('#img');
-    create = document.querySelector('#generate');
-    meme = document.querySelector('#meme');
+  texttop = document.querySelector("#text-top");
+  textbottom = document.querySelector("#text-bottom");
+  img = document.querySelector("#img");
+  create = document.querySelector("#generate");
+  meme = document.querySelector("#meme");
 
-    console.log(texttop)
-    console.log(textbottom)
-    console.log(img)
-    console.log(create)
-    console.log(meme)
-})()
+  meme.widtch = meme.height = 0;
+
+  create.addEventListener("click", () => {
+    let file = new FileReader();
+
+    file.readAsDataURL(img.files[0]);
+  });
+})();
